386cc6102dc358d8925c69aaed78e52f2ffe618c
Binh Nguyen Van <nvbinhsoft@gmail.com>
2025-04-16 09:56:11 +0700

    feat: implement initial Todo portlet structure and resources

---
+++ modules/todo/todo-web/src/main/java/com/example/todo/web/constants/TodoPortletKeys.java
@@ -0,0 +1,11 @@
+package com.example.todo.web.constants;
+
+/**
+ * @author nvbinhsoft
+ */
+public class TodoPortletKeys {
+
+	public static final String TODO =
+		"com_example_todo_web_TodoPortlet";
+
+}
